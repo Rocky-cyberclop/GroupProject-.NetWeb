53d5a3b909a3604580f6859c9cf1f9f11be15320
Rocky-cyberclop <rockyoperationgmail.com>
2023-03-04 11:11:01 +0700

    Create admin area

---
+++ GroupProject/Areas/Admin/Controllers/StatisticController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DAL.FrameWork;
+
+namespace GroupProject.Areas.Admin.Controllers
+{
+    public class StatisticController : Controller
+    {
+        // GET: Admin/Statistic
+        public ActionResult Index()
+        {
+            string label = "['January', 'February', 'March', 'April', 'May', 'June']";
+            ViewBag.label = label;
+            return View();
+        }
+    }
+}

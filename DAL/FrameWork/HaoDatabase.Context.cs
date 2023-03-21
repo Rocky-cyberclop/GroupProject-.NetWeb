fb43b3cec39fd72c60b7b17c5b0155df6f7bd0ec
Rocky-cyberclop <rockyoperationgmail.com>
2023-03-21 14:50:09 +0700

    Complete management with add, edit, delete staff and product

--- DAL/FrameWork/HaoDatabase.Context.cs
+++ DAL/FrameWork/HaoDatabase.Context.cs
@@ -25,6 +25,7 @@ namespace DAL.FrameWork
             throw new UnintentionalCodeFirstException();
         }
     
+        public virtual DbSet<HinhAnh> HinhAnhs { get; set; }
         public virtual DbSet<HoaDon> HoaDons { get; set; }
         public virtual DbSet<KhachHang> KhachHangs { get; set; }
         public virtual DbSet<Loai> Loais { get; set; }
@@ -32,7 +33,6 @@ namespace DAL.FrameWork
         public virtual DbSet<SanPham> SanPhams { get; set; }
         public virtual DbSet<ChiTietHoaDon> ChiTietHoaDons { get; set; }
         public virtual DbSet<GioHang> GioHangs { get; set; }
-        public virtual DbSet<HinhAnh> HinhAnhs { get; set; }
         public virtual DbSet<KhuyenMai> KhuyenMais { get; set; }
     }
 }
--- GroupProject/Areas/Admin/Controllers/ManagementController.cs
+++ GroupProject/Areas/Admin/Controllers/ManagementController.cs
@@ -20,7 +20,7 @@ namespace GroupProject.Areas.Admin.Controllers
         [HttpGet, ActionName("Staffs")]
         public ActionResult StaffsList()
         {
-            var listStaff = from ts in db.NhanViens select ts;
+            var listStaff = from ts in db.NhanViens where ts.Nghi==false select ts;
             return View(listStaff);
         }
 
@@ -66,18 +66,121 @@ namespace GroupProject.Areas.Admin.Controllers
             return RedirectToAction("Staffs");
         }
 
+        [HttpPost, ActionName("DeleteStaff")]
+        [ValidateInput(false)]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteStaff(string id)
+        {
+            NhanVien staff = db.NhanViens.Where(x => x.MaNV == id).SingleOrDefault();
+            staff.Nghi = true;
+            db.SaveChanges();
+            return RedirectToAction("Staffs");
+        }
+
         //Products management
+        [HttpGet, ActionName("AddType")]
+        public ActionResult AddType()
+        {
+            return View();
+        }
+
+        [HttpPost, ActionName("AddType")]
+        public ActionResult AddType(Loai model)
+        {
+            db.Loais.Add(model);
+            db.SaveChanges();
+            return RedirectToAction("Products");
+        }
+
         [HttpGet, ActionName("Products")]
         public ActionResult ProductsList()
         {
-            var listProduct = from ps in db.SanPhams select ps;
+            var listProduct = from ps in db.SanPhams where ps.KhongBan==false select ps;
             return View(listProduct);
         }
 
         [HttpGet, ActionName("CreateProduct")]
         public ActionResult CreateProduct()
         {
+            var listType = from ls in db.Loais select ls;
+            List<Loai> listTypeList = listType.ToList();
+            SelectList lsType = new SelectList(listTypeList, "MaLoai", "TenLoai");
+            ViewBag.lsType = lsType;
             return View();
+        }
+
+        [HttpPost, ActionName("CreateProduct")]
+        [ValidateInput(false)]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateProduct(SanPham model)
+        {
+            var type = Request["type"].ToString();
+            var product = db.SanPhams.Where(ps=>ps.MaSP.StartsWith(type));
+            model.MaSP = type + (product.Count() + 1).ToString();
+            model.KhongBan = false;
+            db.SanPhams.Add(model);
+            db.SaveChanges();
+            var randomName = DateTime.Now.ToBinary().ToString();
+            var name = "";
+            for (var i = 0; i < Request.Files.Count; i++)
+            {
+                if (Request.Files[i].FileName.Contains(".png") || Request.Files[i].FileName.Contains(".jpg"))
+                {
+                    //Making radom name and save to server
+                    name = randomName + "_" + (i+1).ToString() + Request.Files[i].FileName.ToString().Substring(Request.Files[i].FileName.IndexOf('.'));
+                    var pathFile = Server.MapPath("~/Assets/Products/Images/" + name);
+                    Request.Files[i].SaveAs(pathFile);
+
+                    //Save to database
+                    HinhAnh pic = new HinhAnh();
+                    pic.MaSP = model.MaSP;
+                    pic.Hinh = name;
+                    db.HinhAnhs.Add(pic);
+                    db.SaveChanges();
+                }
+            }
+
+            return RedirectToAction("Products");
+        }
+
+        [HttpGet, ActionName("EditProduct")]
+        public ActionResult EditProduct(string id)
+        {
+            var product = from ps in db.SanPhams where ps.MaSP == id select ps;
+            return View(product.FirstOrDefault());
+        }
+
+        [HttpPost, ActionName("EditProduct")]
+        [ValidateInput(false)]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditProductSave(string id)
+        {
+            var product = db.SanPhams.Find(id);
+            TryUpdateModel(product, "", new string[] { "Ten", "XuatXu", "DonVi", "SoLuong", "Gia", "MoTa" });
+            db.Entry(product).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Products");
+        }
+
+
+        [HttpPost, ActionName("DeleteProduct")]
+        [ValidateInput(false)]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteProduct(string id)
+        {
+            var path = Server.MapPath("~/Assets/Products/Images/");
+            SanPham product = db.SanPhams.Where(ps => ps.MaSP == id).SingleOrDefault();
+            product.KhongBan = true;
+
+            while(product.HinhAnhs.Count > 0)
+            {
+                path = Server.MapPath("~/Assets/Products/Images/"+product.HinhAnhs.First().Hinh.ToString());
+                System.IO.File.Delete(path);
+                HinhAnh pic = product.HinhAnhs.First();
+                db.HinhAnhs.Remove(pic);
+            }
+            db.SaveChanges();
+            return RedirectToAction("Products");
         }
     }
 }
